6931d82be437a3d799bffd45a05fa6bd3162c0d5
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-07-28 00:43:32 -0300

    refactor(client): use ProfessionalService instead of ProfessionalRepository directly

--- src/main/java/loopwork/backend/client/ClientService.java
+++ src/main/java/loopwork/backend/client/ClientService.java
@@ -3,22 +3,24 @@ package loopwork.backend.client;
 import loopwork.backend.professional.Professional;
 import loopwork.backend.professional.ProfessionalNotFoundException;
 import loopwork.backend.professional.ProfessionalRepository;
+import loopwork.backend.professional.ProfessionalService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
 
     private final ClientRepository repository;
-    private final ProfessionalRepository professionalRepository;
+    private final ProfessionalService professionalService;
 
-    public ClientService(ClientRepository repository, ProfessionalRepository professionalRepository) {
+    public ClientService(ClientRepository repository, ProfessionalService professionalService) {
         this.repository = repository;
-        this.professionalRepository = professionalRepository;
+        this.professionalService = professionalService;
     }
 
     public ClientResponse create(ClientRequest clientRequest, String professionalId) {
-        Professional professional = professionalRepository.findById(professionalId)
-                .orElseThrow( () -> new ProfessionalNotFoundException(professionalId));
+        Professional professional = professionalService.findByIdOrThrow(professionalId);
 
         if (repository.existsByProfessionalIdAndEmail(professionalId, clientRequest.email())) {
             throw new ClientEmailAlreadyInUseException(clientRequest.email(), professionalId);
@@ -33,5 +35,14 @@ public class ClientService {
         Client savedClient = repository.save(client);
 
         return ClientResponse.fromEntity(savedClient);
+    }
+
+    public List<ClientResponse> getClientsOfProfessional(String professionalId) {
+        professionalService.findByIdOrThrow(professionalId);
+
+        return repository.findByProfessionalId(professionalId)
+                .stream()
+                .map(ClientResponse::fromEntity)
+                .toList();
     }
 }
