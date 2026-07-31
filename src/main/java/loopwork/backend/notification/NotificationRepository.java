839f2e94a83c1160a0bf8f95c7b3ace96b107882
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-07-30 23:27:53 -0300

    feat(recurringSession, occurrence, notification): add JPA Repositories

---
+++ src/main/java/loopwork/backend/notification/NotificationRepository.java
@@ -0,0 +1,9 @@
+package loopwork.backend.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, String> {
+    List<Notification> findByStatus(NotificationStatus status);
+}
